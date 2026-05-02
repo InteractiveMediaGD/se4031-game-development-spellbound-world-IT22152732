fc596eb36d29772ab38e0953bc6c80293ea0cdc7
Hasara Samadhi Herath <127375343+IT22152732@users.noreply.github.com>
2026-05-02 20:39:34 +0530

    add items

--- SpellboundFairyGardenVR/Assets/Scripts/SimpleXRKeyboardMove.cs
+++ SpellboundFairyGardenVR/Assets/Scripts/SimpleXRKeyboardMove.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class SimpleXRKeyboardMove : MonoBehaviour
 {
-    public float moveSpeed = 3f;
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 100f;
     public Transform cameraTransform;
 
     void Update()
     {
         if (cameraTransform == null) return;
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal"); // A/D
+        float z = Input.GetAxis("Vertical");   // W/S
 
         Vector3 forward = cameraTransform.forward;
         Vector3 right = cameraTransform.right;
@@ -20,5 +21,16 @@ public class SimpleXRKeyboardMove : MonoBehaviour
 
         Vector3 move = (forward.normalized * z + right.normalized * x) * moveSpeed * Time.deltaTime;
         transform.position += move;
+
+        // Rotation using Q and E
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
     }
 }
